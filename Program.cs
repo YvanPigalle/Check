c15ab9f3c3ce67d0cae7437806aaa0baf8dab97d
Yvan <130596153+YvanPigalle@users.noreply.github.com>
2023-09-27 00:20:39 +0300

    2nd void

--- Program.cs
+++ Program.cs
@@ -6,3 +6,16 @@
     }
     Console.WriteLine();
 }
+
+void ArrayChanged(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
